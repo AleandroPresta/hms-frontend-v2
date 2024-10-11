e0b00307de81d621ff7be7560518ab19722b69ba
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-11 17:29:02 +0200

    Created rooms module and home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Default route
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'rooms', component: RoomsListComponent },
+];
