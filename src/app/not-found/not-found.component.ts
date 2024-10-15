8e0bee2525169bbc6f1888e11032fd9ee0160f54
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-15 13:55:11 +0200

    Added link to about section

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RoomsHeaderComponent } from '../rooms/rooms-header/rooms-header.component';
+import { RoomsFooterComponent } from '../rooms/rooms-footer/rooms-footer.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
   imports: [
-    RoomsHeaderComponent
+    RoomsHeaderComponent,
+    RoomsFooterComponent
   ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
