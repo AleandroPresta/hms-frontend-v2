738877302fe4a6a0a003014273e28b2bd3ec8c11
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-15 09:53:54 +0200

    Solved bugs on GalleriaModule

--- src/app/rooms/room-detail/room-detail.component.ts
+++ src/app/rooms/room-detail/room-detail.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Room } from '../Room';
+import { GalleriaModule } from 'primeng/galleria';
+
+interface GalleriaItem {
+  itemImageSrc: string;
+  thumbnailImageSrc: string;
+}
 
 @Component({
   selector: 'app-room-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    GalleriaModule
+  ],
   templateUrl: './room-detail.component.html',
-  styleUrl: './room-detail.component.css'
+  styleUrl: './room-detail.component.css',
 })
-export class RoomDetailComponent {
+export class RoomDetailComponent implements OnInit {
+  constructor() { }
 
+  room: Room = {
+    id: 10,
+    name: 'Pulsar Penthouse',
+    type: 'Penthouse',
+    capacity: 6,
+    location: 'Pulsar Wing',
+    images: [
+      'https://picsum.photos/seed/pulsar1/400/400',
+      'https://picsum.photos/seed/pulsar2/400/400',
+      'https://picsum.photos/seed/pulsar3/400/400',
+    ],
+    size: 150,
+    features: ['Three King Beds', 'Pulsar View', 'Private Gym'],
+    bookings: [
+      {
+        id: 11,
+        roomId: 10,
+        startDate: new Date('2023-12-10T14:00:00'),
+        endDate: new Date('2023-12-15T11:00:00'),
+      },
+    ],
+    price: 450,
+    rating: 5,
+  };
+
+  galleryImages: GalleriaItem[] = [];
+
+  ngOnInit(): void {
+    this.galleryImages = this.room.images.map(
+      (imageUrl) => ({
+        itemImageSrc: imageUrl,
+        thumbnailImageSrc: imageUrl,
+      })
+    );
+  }
+
+  onActiveIndexChange(index: number) {
+    console.log('Active image changed:', this.galleryImages[index]);
+    // You can perform any action here when the active index changes
+  }
 }
