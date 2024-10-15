9dae9090e9f8684d56a5ec4fcd0f26109d00cd93
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-15 14:09:47 +0200

    Finished abour card

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RoomsHeaderComponent } from '../rooms/rooms-header/rooms-header.component';
+import { RoomsFooterComponent } from '../rooms/rooms-footer/rooms-footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    RoomsHeaderComponent,
+    RoomsFooterComponent,
+    FontAwesomeModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  faEnvelope = faEnvelope;
+  faLinkedinIn = faLinkedinIn;
+  faGithub = faGithub;
 
 }
