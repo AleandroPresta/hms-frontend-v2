1576c8560c0250b35f2dd034735b95b918b15e5d
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-18 15:56:36 +0200

    Solved routing problem in header

--- src/app/auth/content/content.component.ts
+++ src/app/auth/content/content.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RoomsFooterComponent } from '../../rooms/rooms-footer/rooms-footer.component';
 import { RoomsHeaderComponent } from '../../rooms/rooms-header/rooms-header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-content',
   standalone: true,
   imports: [
     RoomsHeaderComponent,
-    RoomsFooterComponent],
+    RoomsFooterComponent,
+    RouterOutlet],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
