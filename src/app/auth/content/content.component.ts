0e7c9f44c17759c16bb1f43fee95a2c535d2ae74
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-18 15:42:09 +0200

    Added link in header for authentication

--- src/app/auth/content/content.component.ts
+++ src/app/auth/content/content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RoomsHeaderComponent } from "../../rooms/rooms-header/rooms-header.component";
-import { RoomsFooterComponent } from "../../rooms/rooms-footer/rooms-footer.component";
+import { RoomsFooterComponent } from '../../rooms/rooms-footer/rooms-footer.component';
+import { RoomsHeaderComponent } from '../../rooms/rooms-header/rooms-header.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [RoomsHeaderComponent, RoomsFooterComponent],
+  imports: [
+    RoomsHeaderComponent,
+    RoomsFooterComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
