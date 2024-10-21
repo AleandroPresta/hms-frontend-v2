3b56277e0551995adf820300f909e5e8269b7a69
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-21 15:24:52 +0200

    Successfully added custom toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
-    provideClientHydration()]
+    provideClientHydration(),
+    provideNoopAnimations(),
+  ],
 };
