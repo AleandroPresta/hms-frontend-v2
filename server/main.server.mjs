f83abe2723bb67aa0d0316f61856827249fcab40
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-21 11:50:42 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Q24ERTZ2.mjs";import"./chunk-J6FSKMBP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-2JDRUDMQ.mjs";import"./chunk-J6FSKMBP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
