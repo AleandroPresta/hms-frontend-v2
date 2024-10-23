a7aab93d3f9cac374f3e871669683c20a7f9a6e2
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-23 11:10:03 +0200

    Styled admin login form

--- src/app/admin/admin-login-form/admin-login-form.component.ts
+++ src/app/admin/admin-login-form/admin-login-form.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './admin-login-form.component.html',
   styleUrl: './admin-login-form.component.css'
 })
 export class AdminLoginFormComponent {
+
+  @Output() onSubmitLoginEvent = new EventEmitter();
+
+  @Input() incorrectUsernameOrPassword: boolean = false;
+
+  username: string = "";
+  password: string = "";
+
+  onSubmitLogin() {
+    this.onSubmitLoginEvent.emit({ "username": this.username, "password": this.password });
+  }
 
 }
