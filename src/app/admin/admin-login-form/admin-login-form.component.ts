5c82fa1ec43545b3109f5ffd63d0d0df3163a894
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-23 11:06:14 +0200

    Created basic structure of admin component

---
+++ src/app/admin/admin-login-form/admin-login-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-login-form.component.html',
+  styleUrl: './admin-login-form.component.css'
+})
+export class AdminLoginFormComponent {
+
+}
--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { AdminLoginFormComponent } from "./admin-login-form/admin-login-form.component";
+import { NgIf } from '@angular/common';
+import { AdminDashboardComponent } from "./admin-dashboard/admin-dashboard.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+    AdminLoginFormComponent,
+    NgIf,
+    AdminDashboardComponent
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+
+  componentToShow = 'auth';
+
+  showComponent(componentName: string) {
+    this.componentToShow = componentName
+  }
 
 }
