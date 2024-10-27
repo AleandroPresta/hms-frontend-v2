11cc16a6e5ce9de94d5574a51c877a7ef563da8e
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-27 17:22:29 +0100

    Fixed: message appears when no room is found

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Room, Booking } from '../Room';
 import { RoomsSearchComponent } from '../rooms-search/rooms-search.component';
 import { RoomPreviewComponent } from "../room-preview/room-preview.component";
-import { NgClass, NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { RoomFilterComponent } from "./room-filter/room-filter.component";
 
@@ -12,7 +12,7 @@ import { RoomFilterComponent } from "./room-filter/room-filter.component";
   imports: [
     RoomsSearchComponent,
     RoomPreviewComponent,
-    NgFor, NgClass,
+    NgFor, NgClass, NgIf,
     RouterLink,
     RoomFilterComponent
   ],
