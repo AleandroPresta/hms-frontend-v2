18464356ac1a3b963a4f0bae22a8d59a90350836
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-10-27 15:36:26 +0100

    Correcly implemented admin logic (loading, not-admin, dashboard)

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { AdminDashboardComponent } from "./admin-dashboard/admin-dashboard.component";
 import { LoadingComponent } from "./loading/loading.component";
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -18,16 +19,33 @@ export class AdminComponent implements OnInit {
 
   componentToShow: string = 'loading';
   notAdmin = false;
-  role: string = "";
+  role: string | null = "";
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   showComponent(componentName: string) {
     this.componentToShow = componentName
   }
 
   ngOnInit(): void {
-    this.componentToShow = 'loading';
+    this.showComponent('loading');
+    // Get the role of the user from memory
+    const user = this.authService.getLoggedInUser();
+    this.role = user.role;
+
+    // Manage the content to show based on the role
+    this.manageContentToShow(this.role);
+
+  }
+
+  manageContentToShow(role: string | null): void {
+    if (this.role != 'admin') {
+      this.notAdmin = true;
+      this.showComponent('not-admin')
+    } else {
+      this.showComponent('dashboard');
+    }
+
   }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { get } from 'http';
 
 @Injectable({
   providedIn: 'root'
